45e24f8a70882c88b4c3898753e1521eac1b46e0
holdennekt <nikita.spaider2003@gmail.com>
2021-02-01 12:16:13 +0200

    bug fixes

--- graficDrawer/script.js
+++ graficDrawer/script.js
@@ -12,6 +12,10 @@ ctx.lineWidth = 0.08;
 ctx.fillStyle = 'grey';
 ctx.font = "0.4px Arial";
 
+const log = (base, x) => Math.log(x) / Math.log(base);
+const {sin, cos, tan, PI} = Math;
+const e = Math.E;
+
 const line = (fromX, fromY, toX, toY) => {
     ctx.beginPath();
     ctx.moveTo(fromX, fromY);
@@ -19,48 +23,46 @@ const line = (fromX, fromY, toX, toY) => {
     ctx.stroke();
 };
 
-const log = (base, x) => Math.log(x) / Math.log(base);
-
 const drawGrid = () => {
-    ctx.beginPath();
     line(- countX / 2, 0, countX / 2, 0);
     line(0, - countY / 2, 0, countY / 2);
-    for (let i = 1 - countX / 2, j = -1 + countX / 2; i < countX / 2; i++, j++) {
+    for (let i = 1 - countX / 2, j = 1 - countX / 2; i < countX / 2; i++, j++) {
         line(i, -0.2, i, 0.2);
-        // if (j === 0) {
-        //     console.log('j === 0');
-        //     continue;
-        // }
-        ctx.fillText(j.toString(), i - 0.16, 0.6);
+        if (j === 0) continue;
+        ctx.fillText('1', i, 0.6);
+        console.log('fillled');
     }
     for (let i = 1 - countY / 2, j = -1 + countY / 2; i < countY / 2; i++, j--) {
         line(-0.2, i, 0.2, i);
-        //if (j === 0) continue;
-        ctx.fillText(j.toString(), 0.6, i + 0.16);
+        if (j === 0) continue;
+        ctx.fillText(j.toString(), 0.6, i);
     }
 };
 
 const draw = (formula) => {
     let x = - countX / 2;
     ctx.beginPath();
-    ctx.moveTo(x, -eval(formula));
+    ctx.moveTo(x, -1 * eval(formula));
     for (let x = 0.04 - countX / 2; x <= countX / 2; x+= 0.04) {
-        ctx.lineTo(x, -eval(formula));
+        ctx.lineTo(x, -1 * eval(formula));
     }
     ctx.stroke();
 };
 
-button.onclick = () => {
-    let formula = input.value;
-    formula = formula.replace('^', '**');
-    formula = formula.replace('PI', `${Math.PI}`);
-    formula = formula.replace('e', `${Math.E}`);
-    if (formula.includes('sin(')) {
-        let cof = formula.slice(formula.indexOf('sin') + 4, formula.indexOf(')'));
-        let newer = 'Math.sin(' + cof + ')';
-        formula = formula.replace(/sin\(.+\)/, newer);
+const getFormula = (str) => {
+    let formula = str;
+    formula = formula.replace(/\^/g, '**');
+    if (formula.includes('ctg(')) {
+        let cof = formula.slice(formula.indexOf('ctg(') + 4, formula.indexOf(')'));
+        let newer = '(1 / tan(' + cof + '))';
+    formula = formula.replace(/ctg\([^)]+\)/g, newer);
     }
     console.log(formula);
+    return formula;
+};
+
+button.onclick = () => {
+    let formula = getFormula(input.value);
     draw(formula);
 };
 
@@ -68,6 +70,3 @@ clear.onclick = () => {
     ctx.clearRect(-16, -16, 32, 32);
     drawGrid();
 };
-
-//sin(x) sin( x) sin(2 * x) sin(16*x)
-// /sin\(\d*\s*\*\s*x\)/
